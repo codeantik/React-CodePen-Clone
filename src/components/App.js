eb054bfc3b2fcd7c82deecc568d07317e738e82c
Ankit <ankit.sin099@gmail.com>
2021-01-19 12:41:12 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+import Editor from './Editor'
+import React, { useState, useEffect } from 'react'
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+function App() {
+
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+
+  }, [html, css, js])
+
+ 
+
+  return (
+    <>
+      <div className='pane top-pane'>
+        <Editor
+          language='xml'
+          displayName='HTML'
+          value={html}
+          onChange={setHtml}
+         />
+        <Editor 
+          language='css'
+          displayName='CSS'
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language='javascript'
+          displayName='JavaScript'
+          value={js}
+          onChange={setJs}
+         />
+      </div>
+      <div className='pane'>
+        <iframe
+          srcDoc={srcDoc}
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder='0'
+          height='100%'
+          width='100%'
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+
+function Editor(props) {
+
+    const {
+        language,
+        displayName,
+        value,
+        onChange
+    } = props
+
+    const [open, setOpen] = useState(true)
+
+    function handleChange(editor, data, value) {
+        onChange(value)
+    }
+
+    return (
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+            <div className='editor-title'>
+                {displayName}
+                <button
+                    type='button'
+                    className='expand-collapse-btn'
+                    onClick={() => setOpen(prevOpen => !prevOpen)}
+                >
+                    <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+                </button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className='codemirror-wrapper'
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    theme: 'material',
+                    lineNumbers: true
+                }}
+            />
+        </div>
+    )
+}
+
+export default Editor
